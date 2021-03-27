32725e1bce823b017b53f4a93104e41a604fb47c
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-27 22:05:44 +0300

    now the pics go on as I click, but I need to wrap things up

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-// //so this is possible
-
-// const element = document.getElementById('try');
-
-// window.onload = element.style.visibility = 'visible';
-const container = document.getElementById('container');
+// select the individual image like text items
 const items = document.querySelectorAll('.item');
+// get the "right" arrow button
 const rightButton = document.getElementById('right');
+// right button event listener
 rightButton.addEventListener('click', () => {
   for (let i = 0; i < items.length; i++) {
-    console.log(getComputedStyle(items[i]).display);
+    // console.log(getComputedStyle(items[i]).display);
+    // if it's computed style is block...
+    if (getComputedStyle(items[i]).display === 'block') {
+      console.log(items[i].textContent);
+      items[i].style.display = 'none';
+      console.log(getComputedStyle(items[i + 1]).display);
+      console.log(items[i + 1].textContent);
+      items[i + 1].style.display = 'block';
+      return;
+    }
   }
 });
 
