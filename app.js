539807c92469048280200ed0a4355a4a5485ba03
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-27 21:51:38 +0300

    managed to get the elements computed styles in a loop

--- app.js
+++ app.js
@@ -3,3 +3,15 @@
 // const element = document.getElementById('try');
 
 // window.onload = element.style.visibility = 'visible';
+const container = document.getElementById('container');
+const items = document.querySelectorAll('.item');
+const rightButton = document.getElementById('right');
+rightButton.addEventListener('click', () => {
+  for (let i = 0; i < items.length; i++) {
+    console.log(getComputedStyle(items[i]).display);
+  }
+});
+
+// if (items[i].style.display == 'none') {
+//     console.log(items[i].textContent);
+//   }
